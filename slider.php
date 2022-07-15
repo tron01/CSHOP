4cd2e2a167d92eb43f5537d50d07ae1b5e624f01
tron01 <jangoovasuu0789@gmail.com>
2022-07-16 00:00:44 +0530

    banner bootstrap update

--- slider.php
+++ slider.php
@@ -2,22 +2,30 @@
 ?>
 
  <!--slider area-->
- <div class="slider-area">
-      <div class="slider">
-        <!--slide1-->
-        <div><a href="#">
-            <img src="img/banner.jpeg"></a>
-          <div class="slider-content">
-            <h3 class="text-white text-capitalize"></h3>
-          </div>
-        </div>
-        <!-- end slide1-->
-        <div><a href="#">
-            <img src="img/msi-gk20-main-banner.jpeg"></a>
-        </div>
-        <div><a href="#"><img src="img/banner.jpeg"></a>
-        </div>
-      </div>
-    
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/banner.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/banner.png" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
 </div>
     <!--end slider area-->
